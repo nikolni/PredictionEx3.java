3ee3a6496371345aad77661edda5595ed679533f
Maayan56 <maayansha12@gmail.com>
2023-08-25 20:30:10 +0300

    property body and selectItem in body

--- ui-java -fx/src/screen1/body/BodyController.java
+++ ui-java -fx/src/screen1/body/BodyController.java
@@ -5,17 +5,25 @@ import dto.definition.property.definition.api.PropertyDefinitionDTO;
 import dto.definition.rule.api.RuleDTO;
 import dto.definition.termination.condition.manager.api.TerminationConditionsDTOManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import screen1.body.entity.property.PropertyController;
+import screen1.body.entity.property.PropertyResourceConstants;
 import system.engine.api.SystemEngineAccess;
 import system.engine.impl.SystemEngineAccessImpl;
 import dto.api.*;
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,18 +33,20 @@ public class BodyController implements Initializable{
     @FXML
     private TreeView<String> detailesTreeView;
     @FXML
-    private Label valueDefLabel;
+    private Label valueDefLabel;  //for population
 
     @FXML
-    private Label quantityOfSquaresLabel;
+    private Label quantityOfSquaresLabel; //for quantity of properties
 
     @FXML
     private TextFlow valueDefText;
 
     @FXML
     private TextFlow quantityOfSquaresText;
+    @FXML
+    private FlowPane detailesFlowPane;
 
-    SystemEngineAccess systemEngine = new SystemEngineAccessImpl();
+    private SystemEngineAccess systemEngine = new SystemEngineAccessImpl();
 
     public BodyController(){
         try {
@@ -54,18 +64,79 @@ public class BodyController implements Initializable{
         TreeItem<String> entitiesBranch = createEntitiesSubTree(systemEngine);
         TreeItem<String> ruleBranch = new TreeItem<>("Rules");
         TreeItem<String> terminationBranch = new TreeItem<>("Termination conditions");
+        TreeItem<String> environmentBranch = new TreeItem<>("Environment Variables");
 
-        rootItem.getChildren().addAll(entitiesBranch, ruleBranch,terminationBranch);
+        rootItem.getChildren().addAll(entitiesBranch, ruleBranch,terminationBranch,environmentBranch);
         detailesTreeView.setShowRoot(false);
         detailesTreeView.setRoot(rootItem);
     }
-    @FXML
-    void selectItem(ContextMenuEvent event) {
-       /* TreeItem<String> item = detailesTreeView.getSelectionModel().getSelectedItem();
 
-        if(item != null) {
-            System.out.println(item.getValue());
-        }*/
+    private boolean isBranchExpanded(String branchValue,TreeView<String> detailesTreeView) {
+        TreeItem<String> root = detailesTreeView.getRoot();
+        for(TreeItem<String> treeItem:root.getChildren()){
+            if (treeItem.getValue().equals(branchValue))
+                return treeItem.isExpanded();
+        }
+        return false;
+    }
+
+    @FXML
+    void selectItem(MouseEvent event) {
+        if(isBranchExpanded("Entities",detailesTreeView)){
+            showEntityDefDetailes();
+        }
+    }
+
+    public void showEntityDefDetailes(){
+        quantityOfSquaresLabel.setVisible(false);
+        quantityOfSquaresText.setVisible(false);
+        detailesTreeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            valueDefText.getChildren().clear(); // Clear existing text
+            if (newValue != null) {
+                String selectedValue = newValue.getValue();
+                DTODefinitionsForUi dtoDefinitionsForUi = systemEngine.getDefinitionsDataFromSE();
+                List<EntityDefinitionDTO> entities = dtoDefinitionsForUi.getEntitiesDTO();
+                for(EntityDefinitionDTO entityDefinitionDTO : entities){
+                    if(entityDefinitionDTO.getUniqueName().equals(selectedValue)){
+                        //population
+                        valueDefLabel.setText("entity's population:");
+                        Text entityPopulation = new Text(String.valueOf(entityDefinitionDTO.getPopulation()));
+                        valueDefText.getChildren().add(entityPopulation);
+                        //property list
+                        for(PropertyDefinitionDTO propertyDefinitionDTO:entityDefinitionDTO.getProps()){
+                            showPropertyDetailes(propertyDefinitionDTO);
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+    public void showPropertyDetailes(PropertyDefinitionDTO propertyDefinitionDTO){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(PropertyResourceConstants.PROPERTY_FXML_RESOURCE);
+            Node singleProperty = loader.load();
+
+            PropertyController propertyController = loader.getController();
+            propertyController.setPropertyNameLabel(propertyDefinitionDTO.getUniqueName());
+            propertyController.setPropertyTypeLabel(propertyDefinitionDTO.getType());
+            propertyController.setPropertyRangeLabel(propertyDefinitionDTO.getRange());
+            propertyController.setPropertyIsRandomLabel(String.valueOf(propertyDefinitionDTO.isRandomInitialized()));
+            detailesFlowPane.getChildren().add(singleProperty);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void showEnvironmentVarsDetailes(){
+        quantityOfSquaresLabel.setVisible(false);
+        quantityOfSquaresText.setVisible(false);
+        valueDefLabel.setVisible(false);
+        valueDefText.setVisible(false);
     }
 
     public TreeItem<String> createEntitiesSubTree(SystemEngineAccess systemEngineAccess){
@@ -85,10 +156,10 @@ public class BodyController implements Initializable{
     public TreeItem<String> createSingleEntitySubTree(EntityDefinitionDTO entityDefinitionDTO){
         TreeItem<String> entityBranch = new TreeItem<>( entityDefinitionDTO.getUniqueName());
 
-        TreeItem<String> leafPopulation = new TreeItem<>("population");
+       /* TreeItem<String> leafPopulation = new TreeItem<>("population");
 
         TreeItem<String> propertiesBranch = createPropertiesSubTree(entityDefinitionDTO.getProps());
-        entityBranch.getChildren().addAll(leafPopulation, propertiesBranch);
+        entityBranch.getChildren().addAll(leafPopulation, propertiesBranch);*/
         return entityBranch;
     }
 
---
+++ ui-java -fx/src/screen1/body/entity/property/PropertyResourceConstants.java
@@ -0,0 +1,8 @@
+package screen1.body.entity.property;
+
+import java.net.URL;
+
+public class PropertyResourceConstants {
+    private static final String PROPERTY_FXML_RESOURCE_IDENTIFIER = "screen1/body/entity/property/property.fxml";
+    public static final URL PROPERTY_FXML_RESOURCE = PropertyResourceConstants.class.getResource(PropertyResourceConstants.PROPERTY_FXML_RESOURCE_IDENTIFIER);
+}
