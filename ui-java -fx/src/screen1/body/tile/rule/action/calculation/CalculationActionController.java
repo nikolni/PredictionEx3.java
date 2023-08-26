563b8c98c805dc4a0155a3cd6cfd5011276e41bd
Maayan56 <maayansha12@gmail.com>
2023-08-26 17:08:39 +0300

    rules and entities selection handle

---
+++ ui-java -fx/src/screen1/body/tile/rule/action/calculation/CalculationActionController.java
@@ -0,0 +1,23 @@
+package screen1.body.tile.rule.action.calculation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CalculationActionController {
+
+        @FXML
+        private Label actionTypeLabel;
+
+        @FXML
+        private Label primaryEntityLabel;
+
+        @FXML
+        private Label resultPropertyNameLabel;
+
+        @FXML
+        private Label expression1Label;
+
+        @FXML
+        private Label expression2Label;
+
+}
