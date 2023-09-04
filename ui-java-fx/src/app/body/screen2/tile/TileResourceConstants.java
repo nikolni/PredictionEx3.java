5122e4ff1f2e2a0968c9c1c024e28f71bd4a35b3
nikolni <nikolni@mta.ac.il>
2023-09-04 19:47:44 +0300

    1. fixing StartButtonController- now flow panes are shown

--- ui-java-fx/src/app/body/screen2/tile/TileResourceConstants.java
+++ ui-java-fx/src/app/body/screen2/tile/TileResourceConstants.java
@@ -5,7 +5,12 @@ import java.net.URL;
 public class TileResourceConstants {
     private static final String ENTITY_FXML_RESOURCE_IDENTIFIER = "/app/body/screen2/tile/entity/entity.fxml";
     public static final URL ENTITY_FXML_RESOURCE = TileResourceConstants.class.getResource(TileResourceConstants.ENTITY_FXML_RESOURCE_IDENTIFIER);
+    private static final String ENTITY_START_BUTTON_FXML_RESOURCE_IDENTIFIER = "/app/body/screen2/tile/entity/start/button/entityStartButton.fxml";
+    public static final URL ENTITY_START_BUTTON_FXML_RESOURCE = TileResourceConstants.class.getResource(TileResourceConstants.ENTITY_START_BUTTON_FXML_RESOURCE_IDENTIFIER);
 
     private static final String ENVIRONMENT_VAR_FXML_RESOURCE_IDENTIFIER = "/app/body/screen2/tile/environment/variable/environmentVariable.fxml";
     public static final URL ENVIRONMENT_VAR_FXML_RESOURCE = TileResourceConstants.class.getResource(TileResourceConstants.ENVIRONMENT_VAR_FXML_RESOURCE_IDENTIFIER);
+
+    private static final String ENVIRONMENT_VAR_START_BUTTON_FXML_RESOURCE_IDENTIFIER = "/app/body/screen2/tile/environment/variable/start/button/environmentVariableStartButton.fxml";
+    public static final URL ENVIRONMENT_VAR_START_BUTTON_FXML_RESOURCE = TileResourceConstants.class.getResource(TileResourceConstants.ENVIRONMENT_VAR_START_BUTTON_FXML_RESOURCE_IDENTIFIER);
 }
